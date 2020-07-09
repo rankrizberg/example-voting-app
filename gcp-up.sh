d9e881ce68951d71a367d51c96405f7b0bbbafd3
levep <lev.epstein@gmail.com>
2020-07-09 21:15:59 +0300

    script commented

--- gcp-up.sh
+++ gcp-up.sh
@@ -5,6 +5,6 @@ export my_cluster=demo-cluster
 gcloud container clusters create $my_cluster --num-nodes 3 --machine-type=n2-standard-2 --zone $my_zone --enable-ip-alias
 gcloud container clusters get-credentials $my_cluster --zone $my_zone
 
-git clone https://github.com/levep/example-voting-app.git
-cd example-voting-app/
+# git clone https://github.com/levep/example-voting-app.git
+# cd example-voting-app/
 kubectl create -f kube-deployment.yml
